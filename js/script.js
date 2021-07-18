db135be3cc21dad545b1a3c75928f11c846a3e9b
srgh <srgh@bk.ru>
2021-07-18 18:24:39 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//  const result = confirm("Are you here?");
+//  if(!result)
+// {alert("I'm so sorry");}
+// else
+// {alert("Very good!")}
+
+// const answer = prompt("Enter anything","");
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers[0] = prompt("Имя");
+// answers[1] = prompt("Возраст");
+// document.write(answers);
+
+const category = 'toys';
+console.log(`http://www.somesite.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет,${user}`);

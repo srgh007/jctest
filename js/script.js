21dc6cea8bc73c2bd1cd833012d2ac2032b35356
srgh <srgh@bk.ru>
2021-07-24 18:45:19 +0300

    Functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+"restrict"
+// создаётся в первую очередь до начала обработки остального скрипта
+// можно вызвать до объявления!
+
+showFirstMessage("Before func");
+
+function showFirstMessage(Text){
+    console.log(Text)
+}
+
+showFirstMessage(prompt("Enter anything",""));
+
+//functional expression - создаётся тогда когда обработчик дойдёт до её вызова
+const someValue = function(){
+    console.log("This is SomeValue");};
+
+someValue();
+
+//стрелочная функция - аналог лямбда-выражения Java
+const anotherFunc = (a,b) => a*b;
+
+console.log(anotherFunc(9,5));
+
+
+
 //  const result = confirm("Are you here?");
 //  if(!result)
 // {alert("I'm so sorry");}
@@ -19,29 +44,29 @@
 
 // alert(`Привет,${user}`);
 
- const numberOfFilms = prompt("How much films u are was seen?","");
+//  const numberOfFilms = prompt("How much films u are was seen?","");
 
-const personalMovieDB = {
-count : numberOfFilms,
-movies : {},
-actors : {},
-genres : [],
-privat : false
-}; 
+// const personalMovieDB = {
+// count : numberOfFilms,
+// movies : {},
+// actors : {},
+// genres : [],
+// privat : false
+// }; 
 
-for(let i=0;i<2;i++){
-const a = prompt("One of last seen films?",""),
-      b = prompt("How many points do u rate?","");
- if(a!=null && b!= null && a != '' && b != '')
-      {personalMovieDB.movies[a] = b;    
-        console.log('done');}
-    else
-   {console.log('error');
-       i--;}
-    }
-// personalMovieDB[c] = d;
+// for(let i=0;i<2;i++){
+// const a = prompt("One of last seen films?",""),
+//       b = prompt("How many points do u rate?","");
+//  if(a!=null && b!= null && a != '' && b != '')
+//       {personalMovieDB.movies[a] = b;    
+//         console.log('done');}
+//     else
+//    {console.log('error');
+//        i--;}
+//     }
+// // personalMovieDB[c] = d;
 
- console.log(personalMovieDB);
+//  console.log(personalMovieDB);
 
 // let num = 0;// +prompt("Enter number, please","");
 

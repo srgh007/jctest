ac1e221f7f1de57a5e489b745732223f82477566
srgh <srgh@bk.ru>
2021-07-26 00:30:45 +0300

    Object

--- js/script.js
+++ js/script.js
@@ -1,119 +1,33 @@
 "restrict"
 
-
-
-// const tst = "125.25qwerty777";
-// console.log(parseInt(tst));
-// console.log(parseFloat(tst));
-
-// const num = Math.round((Math.random()*100)).toString();
-// alert(num);
-
-// console.log(substring(prompt("Enter u name"),0,3));
-// console.log(prompt("Enter word").substring(0,3));
-
-// showFirstMessage("Before func");
-// // создаётся в первую очередь до начала обработки остального скрипта
-// // можно вызвать до объявления!
-// function showFirstMessage(Text){
-//     console.log(Text)
-// }
-
-// function substring(Text,a,b){
-//     return Text.slice(a,b);
-// }
-
-// showFirstMessage(prompt("Enter anything",""));
-
-// //functional expression - создаётся тогда когда обработчик дойдёт до её вызова
-// const someValue = function(){
-//     console.log("This is SomeValue");};
-
-// someValue();
-
-// //стрелочная функция - аналог лямбда-выражения Java для func.expression
-// const anotherFunc = (a,b) => a*b;
-
-// console.log(anotherFunc(9,5));
-
-
-
-// //  const result = confirm("Are you here?");
-// //  if(!result)
-// // {alert("I'm so sorry");}
-// // else
-// // {alert("Very good!")}
-
-// // const answer = prompt("Enter anything","");
-// // console.log(typeof(answer));
-
-// // const answers = [];
-// // answers[0] = prompt("Имя");
-// // answers[1] = prompt("Возраст");
-// // document.write(answers);
-
-// // const category = 'toys';
-// // console.log(`http://www.somesite.com/${category}/5`);
-
-// // const user = "Ivan";
-
-// // alert(`Привет,${user}`);
-
-//  const numberOfFilms = 99;//prompt("How much films u are was seen?","");
-
-// const personalMovieDB = {
-// count : numberOfFilms,
-// movies : {},
-// actors : {},
-// genres : [],
-// privat : false
-// }; 
-
-// for(let i = 0; i<100;i++)
-// {personalMovieDB.genres[i] = "GenreNomber"+Math.round(Math.random()*100);}
-
-//console.log(personalMovieDB);
-
-function fillArray(ArraySize, callback){
-    // for(let i = 0; i<ArraySize;i++)
-    // {personalMovieDB.genres[i] = "GenreNomber"+Math.round(Math.random()*100);}
-    console.log('Its first function ' + ArraySize);
-    callback();
+const obj = {
+width: 1024,
+height: 1024,
+colours: {
+border: 'black',
+bg: 'red'
 }
+};
 
-function done(){
-    console.log("All done!");
+// console.log(obj['colours'].bg);
+// console.log(obj.colours.border);
+
+// delete obj.height
+
+// console.log(obj)
+
+for(let key in obj){
+    // if(obj[key] == Object)
+    if(typeof(obj[key])!='object'){
+    console.log(key + " " + obj[key]);}
+    else{
+    console.log(key + ":");
+    // const obj2 = obj[key];
+    // for(let key2 in obj2){
+    //     console.log(key2 + " " + obj2[key2]);
+    for(let key2 in obj[key])
+    console.log(key2 + " " + obj[key][key2]);
+    }    
+    }
+    
 }
-
-fillArray(20,function(){
-console.log('Its second function');
-});
-
-fillArray(20,done);
-
-// // for(let i=0;i<2;i++){
-// // const a = prompt("One of last seen films?",""),
-// //       b = prompt("How many points do u rate?","");
-// //  if(a!=null && b!= null && a != '' && b != '')
-// //       {personalMovieDB.movies[a] = b;    
-// //         console.log('done');}
-// //     else
-// //    {console.log('error');
-// //        i--;}
-// //     }
-// // // personalMovieDB[c] = d;
-
-// //  console.log(personalMovieDB);
-
-// // let num = 0;// +prompt("Enter number, please","");
-
-// // (num%2===0) ? alert("Четное") : alert("Нечетное") ;
-
-// // while(num!=33){
-// //     num = +prompt("Enter number, please","");
-// //     if (num!=33)
-// //     {alert("Wrong number");}
-// //     else
-// //     {alert("U are lucky");}
-
-// // }

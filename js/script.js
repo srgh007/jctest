bc4411d3e9b2198eff8d21b28f9dfc1c8ea07672
srgh <srgh@bk.ru>
2021-08-09 00:04:00 +0300

    Arrays function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
 "restrict"
 
-const obj = {
-width: 1024,
-height: 1024,
-colours: {
-border: 'black',
-bg: 'red'
-}
-};
+const arr = [11,23,15,7,14];
+
+// for (let val of arr){
+//     console.log(val);
+// }
+arr.sort(function(a,b){return (a-b);});
+
+console.log(arr);
+
+arr.forEach(function(item,i,arr){console.log(`${i} : ${item} in ${arr}`);});
+// const obj = {
+// width: 1024,
+// height: 1024,
+// colours: {
+// border: 'black',
+// bg: 'red'
+// },
+// someMethod: function(){
+//     console.log('TEST');
+// }
+// };
+
+// console.log(Object.keys(obj).length);
+// obj.someMethod();
+
+// const {} 
 
 // console.log(obj['colours'].bg);
 // console.log(obj.colours.border);
@@ -16,18 +34,18 @@ bg: 'red'
 
 // console.log(obj)
 
-for(let key in obj){
-    // if(obj[key] == Object)
-    if(typeof(obj[key])!='object'){
-    console.log(key + " " + obj[key]);}
-    else{
-    console.log(key + ":");
-    // const obj2 = obj[key];
-    // for(let key2 in obj2){
-    //     console.log(key2 + " " + obj2[key2]);
-    for(let key2 in obj[key])
-    console.log(key2 + " " + obj[key][key2]);
-    }    
-    }
+// for(let key in obj){
+//     // if(obj[key] == Object)
+//     if(typeof(obj[key])!='object'){
+//     console.log(key + " " + obj[key]);}
+//     else{
+//     console.log(key + ":");
+//     // const obj2 = obj[key];
+//     // for(let key2 in obj2){
+//     //     console.log(key2 + " " + obj2[key2]);
+//     for(let key2 in obj[key]){
+//     console.log(key2 + " " + obj[key][key2]);
+//     }    
+//     }
     
-}
+// }
